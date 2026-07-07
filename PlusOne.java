5bbad23fbd917ac3a8cdb386d8d58509c3ebe577
kavitha <kavithamuthusamy2k07@gmail.com>
2026-07-07 19:30:30 +0530

    Add files via upload

---
+++ PlusOne.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+    int i=digits.length-1;
+    while(i>=0){
+        if(digits[i]<9){
+            digits[i]++;
+            return digits;
+        }
+        digits[i]=0;
+        i--;
+    }
+  int[] result=new int[digits.length+1];
+  result[0]=1;
+  return result;
+    } 
+    }
